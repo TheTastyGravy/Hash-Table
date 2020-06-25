cad4eb0dd37f13c72e75fe935056481f1985fe05
TheTastyGravy <cnpowerman23@gmail.com>
2020-06-25 14:52:36 +1000

    Add specialisation template for a string key for HashTable

--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -3,7 +3,7 @@
 #include "Entry.h"
 
 
-
+// Generic template
 template<typename KeyT, typename DataT>
 class HashTable
 {
@@ -21,12 +21,12 @@ public:
 	}
 
 
-
+	// Used to access the hash table like an array
 	DataT& operator [](const KeyT& key)
 	{
 		// Cast the key to char* to get its bytes
 		// Use a hashed key as the array's index
-		auto hashedKey = hash((const char*)&key, sizeof(KeyT)) % size;
+		auto hashedKey = hash((const char*)&key, sizeof(key)) % size;
 
 		// Collision handling
 		while (true)
@@ -53,7 +53,7 @@ public:
 		// Return reference to data
 		return data[hashedKey].data;
 	}
-	
+
 	const DataT& operator [](const KeyT& key) const
 	{
 		// Cast the key to char* to get its bytes
@@ -91,8 +91,102 @@ private:
 	// The hash function used
 	HashFunctions::HashFunc(hash);
 
-	// Pointer to an array of type 'T'
+	// Pointer to an array of entries
 	Entry<KeyT, DataT>* data;
+	// The number of elements in the array 'data'
+	unsigned int size;
+};
+
+
+// Specialisation for string key type
+template<typename DataT>
+class HashTable<std::string, DataT>
+{
+public:
+	HashTable(unsigned int size) :
+		hash(HashFunctions::defaultHash),
+		data(new Entry<std::string, DataT>[size]),
+		size(size)
+	{
+	}
+
+	~HashTable()
+	{
+		delete[] data;
+	}
+
+
+	// Used to access the hash table like an array
+	DataT& operator [](const std::string& key)
+	{
+		// c_str returns const char*
+		// Use a hashed key as the array's index
+		auto hashedKey = hash(key.c_str(), (unsigned int)key.size()) % size;
+
+		// Collision handling
+		while (true)
+		{
+			// If the keys match, this is the correct entry
+			if (data[hashedKey].key == key)
+				break;
+
+			// If the entry's flag is not set, it can be taken
+			if (data[hashedKey].valid == false)
+			{
+				// Set flag and key
+				data[hashedKey].valid = true;
+				data[hashedKey].key = key;
+				break;
+			}
+
+
+			// Linear probing, and loop the key
+			hashedKey += 3;
+			hashedKey %= size;
+		}
+
+		// Return reference to data
+		return data[hashedKey].data;
+	}
+
+	const DataT& operator [](const std::string& key) const
+	{
+		// c_str returns const char*
+		// Use a hashed key as the array's index
+		auto hashedKey = hash(key.c_str(), key.size()) % size;
+
+		// Collision handling
+		while (true)
+		{
+			// If the keys match, this is the correct entry
+			if (data[hashedKey].key == key)
+				break;
+
+			// If the entry's flag is not set, it can be taken
+			if (data[hashedKey].valid == false)
+			{
+				// Set flag and key
+				data[hashedKey].valid = true;
+				data[hashedKey].key = key;
+				break;
+			}
+
+
+			// Linear probing, and loop the key
+			hashedKey += 3;
+			hashedKey %= size;
+		}
+
+		// Return reference to data
+		return data[hashedKey].data;
+	}
+
+private:
+	// The hash function used
+	HashFunctions::HashFunc(hash);
+
+	// Pointer to an array of entries
+	Entry<std::string, DataT>* data;
 	// The number of elements in the array 'data'
 	unsigned int size;
 };
